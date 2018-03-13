4605e057bd51eb7b1e0b7ad15fb12923d6a40f3e
Leon Oostrum <loostrum@github.com>
2018-03-13 14:32:49 +0000

    Added YMW16

---
+++ packages/ymw16/package.py
@@ -0,0 +1,20 @@
+from spack import *
+import os
+
+
+class Ymw16(Package):
+    """YMW16 is a model for the Galactic distribution of free electrons"""
+
+    homepage = "www.xao.ac.cn/ymw16/"
+    url = "http://119.78.162.254/dmodel/ymw16_v1.3.2.tar.gz"
+
+    version('1.3.2', 'bf8584cfbebd1006d561b4a460e93a18')
+
+    patch('arts.patch')
+
+    def install(self, spec, prefix):
+        src = os.getcwd()
+        make_ymw16 = which(src + "/make_ymw16")
+        make_ymw16()
+        os.mkdir(prefix+'/bin', 0755)
+        install('ymw16', prefix.bin)
