9bb7ea76ae1fb29def343d1ecb5a83907ac53e7a
Gijs Molenaar <gijs@pythonic.nl>
2018-06-07 16:13:04 +0200

    presto package working

--- packages/presto/package.py
+++ packages/presto/package.py
@@ -1,3 +1,4 @@
+import os
 from spack import *
 
 
@@ -10,10 +11,24 @@ class Presto(MakefilePackage):
     version('2.1', '36b8007b0e8513371a1e886bf79f309c')
 
     depends_on('python')
+    depends_on('fftw')
+    depends_on('cfitsio')
+    depends_on('libpng')
+    depends_on('glib')
+    depends_on('pgplot')
 
     build_directory = "src"
 
+    def setup_environment(self, spack_env, run_env):
+        self.source_path = "{}/{}-{}/".format(self.stage.path, self.name, self.version)
+        spack_env.set('PRESTO',  self.source_path)
+        spack_env.set('LD_LIBRARY_PATH', self.source_path + "/lib")
+        spack_env.set('PGPLOT_DIR', self.spec['pgplot'].prefix)
+
     def install(self, spec, prefix):
-        env['PRESTO'] = "."
-        make()
-        make('install')
+        for i in os.listdir('bin'):
+            install('bin/' + i, prefix.bin)
+
+        for i in os.listdir('lib'):
+            if i.endswith('.so'):
+                install('lib/' + i, prefix.lib)
