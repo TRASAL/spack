2d8faed56d01cfa2e02352d1093301e4451383b7
Gijs Molenaar <gijs@pythonic.nl>
2018-06-08 17:07:57 +0200

    add dadafilterbank package

---
+++ packages/dadafilterbank/package.py
@@ -0,0 +1,12 @@
+from spack import *
+import os
+
+
+class Datafilterbank(CMakePackage):
+    homepage = "https://github.com/AA-ALERT/dadafilterbank"
+
+    version('2016-12-15', git='https://github.com/gijzelaerr/dadafilterbank', commit='5f79b7b9b3f0eeb5220cf487d81d4e8d1ba35e67')
+
+    depends_on('cuda')
+    depends_on('psrdada')
+
