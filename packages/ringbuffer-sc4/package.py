024ce07b68a155fcdbcf427c6e58139f974e9d16
Gijs Molenaar <gijs@pythonic.nl>
2018-06-13 14:24:52 +0200

    switch back to upstream

--- packages/ringbuffer-sc4/package.py
+++ packages/ringbuffer-sc4/package.py
@@ -6,8 +6,9 @@ class RingbufferSc4(CMakePackage):
     homepage = "https://github.com/AA-ALERT/ringbuffer-sc4"
     url = "https://github.com/AA-ALERT/ringbuffer-sc4"
 
-    version('2016-12-15', git='https://github.com/gijzelaerr/ringbuffer-sc4',
-            commit='e77ee0671ef319665f98cd2659f91e2d1da0d363')
+    version('2018-06-13',
+            git='https://github.com/AA-ALERT/ringbuffer-sc4',
+            commit='2a02ac2fb7d944fb81ecb6971fc1cd54811109ea')
 
     depends_on('cuda')
     depends_on('psrdada')
