55252df6d22a88d3e6d7fb70b178ce4abc55c76c
Gijs Molenaar <gijs@pythonic.nl>
2018-06-13 16:42:25 +0200

    set to PR

--- packages/dadafilterbank/package.py
+++ packages/dadafilterbank/package.py
@@ -5,7 +5,9 @@ import os
 class Dadafilterbank(CMakePackage):
     homepage = "https://github.com/AA-ALERT/dadafilterbank"
 
-    version('2016-12-15', git='https://github.com/gijzelaerr/dadafilterbank', commit='5f79b7b9b3f0eeb5220cf487d81d4e8d1ba35e67')
+    version('2018-06-11',
+            git='https://github.com/gijzelaerr/dadafilterbank',
+            commit='5acbe65159ca6dd8d3525fa31d8714815f79af6d')
 
     depends_on('cuda')
     depends_on('psrdada')
