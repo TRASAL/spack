926e1ee6371b7e5728a1a43f53fdba8870317810
Gijs Molenaar <molenaar@arts041.apertif>
2018-06-14 14:38:51 +0000

    fix and update tempo2

--- packages/tempo2/package.py
+++ packages/tempo2/package.py
@@ -5,17 +5,20 @@ class Tempo2(AutotoolsPackage):
     """Tempo2 is a new pulsar timing package."""
 
     homepage = "http://www.atnf.csiro.au/research/pulsar/tempo2/index.php"
-    url      = "https://bitbucket.org/psrsoft/tempo2"
+    url      = "https://bitbucket.org/psrsoft/tempo2/downloads/tempo2-2018.02.1.tar.gz"
 
-    version('2018-01-18', git='https://bitbucket.org/psrsoft/tempo2', commit='b1fe783')
+    version('2018.02.1', '1c9e881c6f8e3c40e6d79a638a702e8c4c09880b')
 
     depends_on('autoconf', type='build')
     depends_on('automake', type='build')
     depends_on('libtool',  type='build')
     depends_on('m4',       type='build')
 
+    def setup_environment(self, spack_env, run_env):
+	run_env.prepend_path('TEMPO2', self.prefix) 
+	spack_env.prepend_path('TEMPO2', self.prefix) 
+
     def install(self, spec, prefix):
-        env['TEMPO2'] = prefix
         make()
         make('plugins')
         make('install')
