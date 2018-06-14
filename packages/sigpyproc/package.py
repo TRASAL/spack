454274c4bbb9088e366b72777fe4ea2fde3942db
Gijs Molenaar <gijs@pythonic.nl>
2018-06-14 14:19:58 +0200

    add sigpyproc package

---
+++ packages/sigpyproc/package.py
@@ -0,0 +1,17 @@
+from spack import *
+import os
+
+
+class Sigpyproc(PythonPackage):
+    #homepage = "https://github.com/ewanbarr/sigpyproc"
+    # switch to a PR until merged https://github.com/ewanbarr/sigpyproc/pull/8
+    homepage = "https://github.com/telegraphic/sigpyproc"
+
+    version('2018-05-30',
+            git=homepage,
+            commit='a18a1e201b69b7babb6dd95f6e994d7317b67dc5')
+
+    depends_on('python')
+    depends_on('py-setuptools')
+    depends_on('py-numpy')
+    depends_on('fftw@3.3:')
