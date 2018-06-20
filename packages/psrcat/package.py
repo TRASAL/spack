1bfff6882841d6145e04200c0bb09094f9cce3bf
Gijs Molenaar <gijs@pythonic.nl>
2018-06-20 10:06:43 +0000

    update checksum and add a bit of doc

--- packages/psrcat/package.py
+++ packages/psrcat/package.py
@@ -3,12 +3,18 @@ import os
 
 
 class Psrcat(Package):
-    """PSRCAT is the ATNF pulsar catalogue"""
+    """
+    PSRCAT is the ATNF pulsar catalogue
+
+    Note that the tarball is updated regularly without version change, so the
+    checksum changes also. If the checksum fails it needs to be updated here,
+    or the package needs to be installed with --no-checksum
+    """
 
     homepage = "http://www.atnf.csiro.au/people/pulsar/psrcat/"
     url = "http://www.atnf.csiro.au/people/pulsar/psrcat/downloads/psrcat_pkg.tar.gz"
 
-    version('1.58', '0928ff35f1856655f9da039e35a94820')
+    version('1.58', 'e1b95491e91d708630d15144eae88357')
 
     def setup_environment(self, spack_env, run_env):
         run_env.prepend_path('PSRCAT_FILE', self.prefix+"/psrcat.db")
