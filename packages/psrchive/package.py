3a06521a9a70d2d3541006a0be669fd71506aaf7
Gijs Molenaar <gijs@pythonic.nl>
2018-06-28 14:54:24 +0000

    add optional X11

--- packages/psrchive/package.py
+++ packages/psrchive/package.py
@@ -10,10 +10,27 @@ class Psrchive(AutotoolsPackage):
             git=url,
             commit='bcc5729fe92d90d8745d7293d632241ea0ec2b16')
 
+    variant("x11", default=False, description="Enable GUI")
+    variant("python", default=False, description="Enable Python interface")
+
     depends_on('tempo2')
     depends_on('pgplot')
     depends_on('fftw')
 
+    depends_on('libx11', when="+x11")
+    depends_on('qt', when="+x11")
+    depends_on("python")
+    depends_on('py-numpy')
+
+    def configure_args(self):
+        args = ['--enable-shared']
+        if "+x11" in self.spec:
+            args.append("--with-x") 
+        return args
+
+    def setup_environment(self, spack_env, run_env):
+        run_env.set('PGPLOT_FONT', self.spec['pgplot'].prefix)
+
     def autoreconf(self, spec, prefix):
-        # don't know how to call a script the spack way
-        check_call("./bootstrap")
+        bootstrap = which(os.getcwd() + '/bootstrap')
+        bootstrap()
