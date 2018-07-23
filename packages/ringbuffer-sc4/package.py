b26e09ecbd21526750b15f0279877ebce706cc45
Leon Oostrum <oostrum@arts041.apertif>
2018-07-23 08:50:37 +0000

    Update ringbuffer-sc4 to latest

--- packages/ringbuffer-sc4/package.py
+++ packages/ringbuffer-sc4/package.py
@@ -8,7 +8,7 @@ class RingbufferSc4(CMakePackage):
 
     version('2018-06-13',
             git='https://github.com/AA-ALERT/ringbuffer-sc4',
-            commit='2a02ac2fb7d944fb81ecb6971fc1cd54811109ea')
+            commit='603814db9767a663b6b6b38bcdbbb91c1f70da3a')
 
     depends_on('cuda')
     depends_on('psrdada')
