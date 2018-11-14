9bbd24247a57169ffcb6524eecb750d96156575f
ARTS <arts@arts041.apertif>
2018-11-14 14:06:26 +0000

    Add vispy and astron version of log4cplus

---
+++ packages/py-vispy/package.py
@@ -0,0 +1,12 @@
+from spack import *
+
+
+class PyVispy(PythonPackage):
+    """VisPy is a Python library for interactive scientific visualization that is designed to be fast, scalable, and easy to use."""
+
+    homepage = "https://www.vispy.org"
+    url      = "https://pypi.io/packages/source/v/vispy/vispy-0.5.3.tar.gz"
+
+    version('0.5.3', "fe24dbeebe0fdc2ab567732e5f3cfdac")
+
+    
