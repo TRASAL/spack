d70c2957320f9b5dd259fb207355cb6626d480d0
ARTS <arts@arts041.apertif>
2018-12-06 11:08:45 +0000

    Add py-wordpress-xmlrpc

---
+++ packages/py-wordpress-xmlrpc/package.py
@@ -0,0 +1,11 @@
+from spack import *
+
+
+class PyWordpressXmlrpc(PythonPackage):
+    """WordPress XML-RPC API Integration Library"""
+    homepage = "http://python-wordpress-xmlrpc.rtfd.org"
+    url = "https://pypi.io/packages/source/p/python-wordpress-xmlrpc/python-wordpress-xmlrpc-2.3.zip"
+
+    version('2.3', "2c6f9a5942f244a376882331616d0923")
+
+
