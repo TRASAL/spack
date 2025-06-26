6422e4f0f4c4791d5d3ffa86be47255ebeb0e24c
Leon Oostrum <leonoostrum@gmail.com>
2025-06-26 17:00:58 +0200

    fix psrchive python bindings

--- packages/psrchive/package.py
+++ packages/psrchive/package.py
@@ -14,17 +14,38 @@ class Psrchive(AutotoolsPackage):
     depends_on('pgplot', when='~x11')
     depends_on('pgplot+X', when='+x11')
     depends_on('fftw')
+    depends_on('gsl')
 
     depends_on('libx11', when="+x11")
     depends_on('qt', when="+x11")
     depends_on("python", when="+python")
     depends_on('py-numpy', when="+python")
+    depends_on('swig', when="+python")
+
+    @run_before("build")
+    def fix_numpy(self):
+        # fix compatibility with numpy 2+
+        if "+python" in self.spec and self.spec['py-numpy'].satisfies("@2:"):
+            new_content = """#include "numpy/ndarrayobject.h"
+#define PyArray_DOUBLE NPY_DOUBLE
+#define PyArray_FLOAT NPY_FLOAT
+"""
+            filter_file('#include "numpy/noprefix.h"', new_content, 'More/python/psrchive.i')
+
+    def setup_build_environment(self, env):
+        # path to python after following symlinks, such that psrchive can find libpython
+        python_path = os.path.abspath(os.path.realpath(which('python').path))
+        env.set('PYTHON', python_path)
+
+    def setup_run_environment(self, env):
+        python_version_major, python_version_minor = self.spec['python'].version.version[0][:2]
+        python_version = f'{python_version_major}.{python_version_minor}'
+        env.append_path('PYTHONPATH', f'{prefix.lib}/python{python_version}/site-packages')
 
     def configure_args(self):
         args = ['--enable-shared']
         if "+x11" in self.spec:
             args.append("--with-x") 
+        if "+python" not in self.spec:
+            args.append("--disable-python")
         return args
-
-    def setup_run_environment(self, env):
-        env.set('PGPLOT_FONT', self.spec['pgplot'].prefix+'/include/grfont.dat')
